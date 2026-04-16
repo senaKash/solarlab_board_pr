c25c75f38fad254e14675506b5cfbaad11795dde
Sena <ku.kuptsov@yandex.ru>
2026-04-16 20:19:08 +0300

    инициализация

---
+++ src/test/java/ru/course/bulletin_board/BulletinBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.course.bulletin_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletinBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
